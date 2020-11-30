5ae5d85b862b19b07b2f87753a0d6eb99f96e2a0
fer-k <ferkas@gmail.com>
2020-11-30 19:20:02 -0300

    no agarra los productos

--- src/Containers/ProductsContainer.js
+++ src/Containers/ProductsContainer.js
@@ -1,42 +1,54 @@
 import React,{useState, useEffect} from 'react';
-import EachProduct from '../Components/EachProduct';
-import ProductList from '../ProductList.json';
+//import EachProduct from '../Components/EachProduct';
+//import ProductList from '../ProductList.json';
 import { GetDB } from '../tools/firebaseFactory';
 
+const getPromise = (data) => {
+    return new Promise((res, rej) => {
+        return res(data)
+    })
+}
 
 const ProductsContainer = () => {
 
-
-const [SourceProducts, setProducts] = useState([]);
-
-    function getAll() {
-        const DB = GetDB();
+    const [SourceProducts, setProducts] = useState([]);
+    const DB = GetDB();
+    const allProducts = DB.collection("item colection");
     
-        const allProducts = DB.collection("item colection");
+
+    useEffect(() => {
+        getPromise(() => {
+            allProducts 
+                .get()
+                .then((result) => {
         
-        allProducts 
-            .get()
-            .then((result) => {
-    
-                if(result.size === 0) {
-                    console.log("no traje nada");
-                }
-    
-                result.docs.forEach((doc) => {
-                    const news = [doc.data(), ...SourceProducts]
-                    setProducts(news);
-                    console.log(news)
-
-            });
-            })
-            .catch((error) => console.log(error))
-            .finally(() => {})
-    }
-
-    getAll();
-    return (
-        <EachProduct/>
-    )
+                    if(result.size === 0) {
+                        console.log("no traje nada");
+                    }
+        
+                    result.docs.map((doc) => {
+                        const news = [doc.data(), ...SourceProducts]
+                        setProducts(news);
+                        
+                    });
+                })
+                .catch((error) => console.log(error))
+                .finally(() => {})
+            })        
+        })
+        
+        console.log(allProducts)
+        return (
+        
+            allProducts.map((item, i) => {
+            
+                return <div key={item.id}>
+                <h3>{item.title}</h3>
+                <p>${item.price}</p>
+                </div>
+            }))
+            //<EachProduct/>
+        
 }
 
 export default ProductsContainer
