0c6617e162f414b8f2931c5e4ada049c872cda2a
ferka <ferkas@gmail.com>
2020-11-30 21:40:32 -0300

    productos traidos desde firebase

--- src/Components/EachProduct.js
+++ src/Components/EachProduct.js
@@ -2,20 +2,35 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import {useCartContext} from '../Context/CartContext'
 
+const getPromise = (data) => {
+    return new Promise((res, rej) => {
+            return res(data)
+        }, 1000)
+}
 
+const EachProduct = ({ProductList}) => {
 
-const EachProduct = () => {
-
-
+    const {addProduct} = useCartContext();
+    
+    const [SourceData, setSourceData] = useState([]);
+    
+    useEffect(() => {
+        getPromise( ProductList ).then(result => setSourceData(result));
+    }, [ProductList])
+    
     return (
-       
-            <div>
-            <h3></h3>
-            <p></p>
+        SourceData.map((item, i) => {
             
+            return <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>${item.price}</p>
+            <Link to={`/${item.id}`}><button>Detalles del Producto</button></Link>
+            <button onClick={() => addProduct(item)}>Agregar al carrito</button>
             </div>
             
-    )
+            
+        }
+        ))
     }
     
     export default EachProduct
--- src/Containers/ProductsContainer.js
+++ src/Containers/ProductsContainer.js
@@ -1,46 +1,43 @@
 import React,{useState, useEffect} from 'react';
-//import EachProduct from '../Components/EachProduct';
+import EachProduct from '../Components/EachProduct';
 //import ProductList from '../ProductList.json';
 import { GetDB } from '../tools/firebaseFactory';
 
+/* 
 const getPromise = (data) => {
     return new Promise((res, rej) => {
         return res(data)
     })
-}
+} */
 
 const ProductsContainer = () => {
 
-    const DB = GetDB();
-    const allProducts = DB.collection("item colection");
     const [SourceProducts, setProducts] = useState([]);
-    
-
     useEffect(() => {
-        getPromise(() => {
-            allProducts 
-                .get()
-                .then((result) => {
-        
-                    if(result.size === 0) {
-                        console.log("no traje nada");
-                    }
-        
-                    result.docs.map((doc) => setProducts(doc.data()));
-                })
-                .catch((error) => console.log(error))
-                .finally(() => {})
-            })        
-        })
-        
+        const DB = GetDB();
+        const allProducts = DB.collection("item colection");
+        allProducts
+            .get()
+            .then((querySnapshot) => {
+                if (querySnapshot.size === 0) {
+                    console.log("no resultados pa")
+                }
+                setProducts(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+            }).catch((error) => {
+                console.log("Error buscando prods", error)
+            }).finally(() => {
+                console.log("listo")
+                console.log(SourceProducts)
+            })
+    },[])
+
         console.log(SourceProducts.length)
 
         return (
-            SourceProducts.map((item, i) => {
-                return <div key={item.id}>
-                    <p>{item.title}</p>
-            </div>
-            }))
+            <div>
+            <EachProduct ProductList={SourceProducts}/>
+           </div>
+           )
 }
 
 export default ProductsContainer
